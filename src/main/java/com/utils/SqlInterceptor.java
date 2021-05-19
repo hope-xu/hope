2e2456ac0947510d8a72931a4da9083b98bf7246
hope-xu <hope-xu@outlook.com>
2021-05-19 17:49:12 +0800

    优化SQL打印输出

--- src/main/java/com/utils/SqlInterceptor.java
+++ src/main/java/com/utils/SqlInterceptor.java
@@ -1,12 +1,19 @@
 package com.utils;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.ParameterMapping;
 import org.apache.ibatis.plugin.*;
 import org.apache.ibatis.session.ResultHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Field;
 import java.sql.Statement;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -25,23 +32,153 @@ public class SqlInterceptor implements Interceptor {
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
         long startTime = System.currentTimeMillis();
-        try{
+        Object target = invocation.getTarget();
+        StatementHandler statementHandler = (StatementHandler) target;
+
+        try {
             return invocation.proceed();
-        }finally {
+        } finally {
             long endTime = System.currentTimeMillis();
+            long sqlCost = endTime - startTime;
+            BoundSql boundSql = statementHandler.getBoundSql();
+            String sql = boundSql.getSql();
+            Object parameterObject = boundSql.getParameterObject();
+            List<ParameterMapping> parameterMappings = boundSql.getParameterMappings();
+
+            sql = formatSql(sql, parameterObject, parameterMappings);
             if (log.isInfoEnabled()) {
-                log.info("sql excute time:{ "+(endTime-startTime)+"ms }");
+                log.info("SQL:[ " + sql + " ],UUID: [ " + endTime + " ]" + "sql excute time:{ " + sqlCost + "ms }");
             }
         }
     }
 
     @Override
     public Object plugin(Object o) {
-        return Plugin.wrap(o,this);
+        return Plugin.wrap(o, this);
     }
 
     @Override
     public void setProperties(Properties properties) {
 
     }
+
+
+    @SuppressWarnings("unchecked")
+    private String formatSql(String sql, Object parameterObject, List<ParameterMapping> parameterMappingList) {
+        if (sql == null || sql.length() == 0) {
+            return "";
+        }
+
+        sql = beautifySql(sql);
+
+        if (parameterObject == null || parameterMappingList == null || parameterMappingList.size() == 0) {
+            return sql;
+        }
+
+        String sqlPlaceholder = sql;
+
+        try {
+            if (parameterMappingList != null) {
+                Class<?> parameterObjectClass = parameterObject.getClass();
+                if (isList(parameterObjectClass)) {
+                    Collection<?> col = (Collection<?>) parameterObject;
+                    sql = handlerListParameter(sql, col);
+                } else if (isMap(parameterObjectClass)) {
+                    Map<?, ?> paramMap = (Map<?, ?>) parameterObject;
+                    sql = handleMapParameter(sql, paramMap, parameterMappingList);
+                } else {
+                    sql = handlerCommonParameter(sql, parameterMappingList, parameterObjectClass, parameterObject);
+                }
+            }
+        } catch (Exception e) {
+            return sqlPlaceholder;
+        }
+        return sql;
+    }
+
+
+    private String beautifySql(String sql) {
+        sql = sql.replaceAll("[\\s\n ]+", " ");
+        return sql;
+    }
+
+    private String handlerListParameter(String sql, Collection<?> col) {
+        if (col != null && col.size() != 0) {
+            for (Object obj : col) {
+                String value = null;
+                Class<?> objClass = obj.getClass();
+                if (isPrimitiveOrPrimitiveWrapper(objClass)) {
+                    value = obj.toString();
+                } else if (objClass.isAssignableFrom(String.class)) {
+                    value = "\"" + obj.toString() + "\"";
+                }
+                sql = sql.replaceFirst("\\?", value);
+            }
+        }
+        return sql;
+    }
+
+
+    private String handleMapParameter(String sql, Map<?, ?> paramMap, List<ParameterMapping> parameterMappingList) {
+        for (ParameterMapping parameterMapping : parameterMappingList) {
+            String propertyName = parameterMapping.getProperty();
+            Object propertyValue = paramMap.get(propertyName);
+            if (propertyValue != null) {
+                if (propertyValue.getClass().isAssignableFrom(String.class)) {
+                    propertyValue = "\"" + propertyValue + "\"";
+                }
+                sql = sql.replaceFirst("\\?", propertyValue.toString());
+            }
+        }
+        return sql;
+    }
+
+    private String handlerCommonParameter(String sql, List<ParameterMapping> parameterMappingList, Class<?> parameterObjectClass, Object parameterObject) throws NoSuchFieldException, IllegalAccessException {
+        for (ParameterMapping parameterMapping : parameterMappingList) {
+            String propertyValue = null;
+            if (isPrimitiveOrPrimitiveWrapper(parameterObjectClass)) {
+                propertyValue = parameterObject.toString();
+            } else {
+                String propertyName = parameterMapping.getProperty();
+                Field field = parameterObjectClass.getDeclaredField(propertyName);
+                field.setAccessible(true);
+                propertyValue = String.valueOf(field.get(parameterObject));
+                if (parameterMapping.getJavaType().isAssignableFrom(String.class)) {
+                    propertyValue = "\"" + propertyValue + "\"";
+                }
+            }
+            sql = sql.replaceFirst("\\?", propertyValue.toString());
+        }
+        return sql;
+    }
+
+
+    private boolean isPrimitiveOrPrimitiveWrapper(Class<?> clazz) {
+        return clazz.isPrimitive() ||
+                (clazz.isAssignableFrom(Byte.class) || clazz.isAssignableFrom(Short.class) ||
+                        clazz.isAssignableFrom(Integer.class) || clazz.isAssignableFrom(Long.class) ||
+                        clazz.isAssignableFrom(Double.class) || clazz.isAssignableFrom(Float.class) ||
+                        clazz.isAssignableFrom(Character.class) || clazz.isAssignableFrom(Boolean.class));
+    }
+
+    private boolean isList(Class<?> clazz) {
+        Class<?>[] interfaceClasses = clazz.getInterfaces();
+        for (Class<?> interfaceClass : interfaceClasses) {
+            if (interfaceClass.isAssignableFrom(List.class)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isMap(Class<?> clazz) {
+        Class<?>[] interfaceClasses = clazz.getInterfaces();
+        for (Class<?> interfaceClass : interfaceClasses) {
+            if (interfaceClass.isAssignableFrom(Map.class)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
